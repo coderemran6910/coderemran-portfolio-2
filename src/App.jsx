a8c14d08edc86062b9f67e8ed631972709878108
Coder Emran <coderemran6910@gmail.com>
2024-01-04 19:09:13 +0600

    Project setup compleate

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './app.scss';
+const App = () => {
+  return <div>
+    <section>Hero</section>
+    <section>Peralax</section>
+    <section>Services</section>
+    <section>Peralax</section>
+    <section>Portfolio 1</section>
+    <section>Portfolio 2</section>
+    <section>Portfolio 3</section>
+    <section>Contact</section>
+  </div>;
+};
+
+export default App;
